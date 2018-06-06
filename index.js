2e5fa608e3471497064d34e4d48d3624a6ae3889
priyasonal <39788144+priyasonal@users.noreply.github.com>
2018-06-06 16:33:31 +0530

    front
    xx

---
+++ index.js
@@ -0,0 +1,20 @@
+var router = express.Router();
+
+var fs = require('fs')
+
+$.get("url", function(data,){
+for(i=0;i<mystring.obj.length;i++)
+{
+
+var mystring = ['<li>',
+            '<h3>'+obj.title+'</h3>',
+            '<img>'+obj.imageurl+'</img>',
+            '<p>'+ obj.description +'</p>',
+            '<button>Accept</button>',
+    '</li>'].join('');
+    
+        $('#newlist').append(mystring)
+    }
+    //$.each(data, function(index, value){
+     //   $("#result").append(index + ": " + value + '<br>');
+   // $('#newlist').append(mystring)
