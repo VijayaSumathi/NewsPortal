32478b9604f179cfc640eafe40edd137783d9099
sumathi <vijaya.sumathi@spotcues.com>
2018-06-07 08:45:49 +0530

    api for approve
    api for approve and delete data

--- routes/index.js
+++ routes/index.js
@@ -3,28 +3,51 @@ var router = express.Router();
 var uploadmynew= require('../models/uploadNews');
 var login= require('../models/adminlogin');
 var approvednews= require('../models/adminapprove');
+const multer = require('multer');
+const path = require('path');
 
 
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.post('/user', function(req, res, next) {  
-  const news = new uploadmynew({
-    title: req.body.title, 
-    description: req.body.description
-});
+router.post('/user', function(req, res, next) { 
+  
+  const storageEngine = multer.diskStorage({
+    destination: __dirname + '/../public/images/',
+    filename: function(req, file, fn){
+      fn(null,file.fieldname+'-'+ Date.now() + path.extname(file.originalname));
+    }
+    
+  });
+  
+  const upload =  multer({
+    storage: storageEngine    
+  }).single('pic');
+
+  upload(req, res, function(err, result) {
+
+    const news = new uploadmynew({
+    //  title: req.body.title, 
+     // description: req.body.description,
+      path:res.req.file.filename
+  
+  });
   news.save()
-    .then(data => {
-        console.log("inserted");
-        //res.send(data);
-    }).catch(err => {
-        res.status(500).send({
-            message: err.message || "Some error occurred while creating the Note."
-        });
-        console.log("error");
-    });
-  res.json({news:news});
+  .then(data => {
+      console.log("inserted");
+      //res.send(data);
+  }).catch(err => {
+      res.status(500).send({
+          message: err.message || "Some error occurred while creating the Note."
+      });
+      console.log("error");
+  });
+   res.json({news:news});
+    console.log('The filename is ' +res.req.file.filename );
+    console.log("!!");
+  });  
+ 
 });
 
 
@@ -41,15 +64,15 @@ router.post('/login', function(req, res, next) {
 login.find({name:username}, function(err, data){
  if(password==data[0].password )
  {
-  res.json({password:"correct"});
+   res.json({password:"correct"});
  }
  else{
   res.json({password:"wrong"});
  }
 
-  console.log(">>>> " + data[0].password );
+  console.log(">>>> " );
   });
-
+  
 });
 
 
@@ -71,14 +94,43 @@ router.post('/approve', function(req, res, next){
   var status1=req.body.status;
   var id1=req.body.id;
   if(status1.toLowerCase()=="accept")
-  {  
+  {    
     
+    uploadmynew.find({_id:id1}, function(err, data){
+      if(err) {res.json(err);}
+    else {      
+       const approvenews = new approvednews({
+        title:data[0].title, 
+        description: data[0].description
+       });
+        
+       approvenews.save(function(err){
+               console.log("inserted");
+               if(err)
+               console.error(err);
+                
+       });        
+      
+       res.json(data);
 
-     res.json({display:"accept"});
+
+      }});  
+      uploadmynew.findOne({_id:id1}, function (error, data){
+        console.log("This object will get deleted " + data);
+        data.remove();
+
+    }); 
+     
+    } 
+    
   
   
-  }
   else{
+    uploadmynew.findOne({_id:id1}, function (error, data){
+      console.log("This object will get deleted " + data);
+      data.remove();
+
+    });
 
     res.json({display:"News rejected"});
   }
