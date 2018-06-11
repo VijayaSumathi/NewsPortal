90de87f559cd3f0a5621e3837a16f14a21ef2a11
Umesh <umeshrastogi007@gmail.com>
2018-06-11 11:33:55 +0530

    knkl

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -3,29 +3,28 @@
 //ourRequest.onload=function(){
 //console.log(ourRequest.responseText);
 //};
-function onAccept(id){
-  console.log(id);
-  $.ajax({
-    type:"post",
-    url:"http//localhost:3000/approval",
-    data:id,
-    success:function(){
-      console.log("data")
-    }
-  })
+function onAccept(id) {
+    console.log(id);
+    $.ajax({
+        type: "post",
+        url: "http//localhost:3000/approval",
+        data: id,
+        success: function() {
+            console.log("data")
+        }
+    })
 }
-$(function(){
-  var $newslist=$('#newslist');
-  $.ajax({
-    type:'GET',
-    url:'/news/all',
-    success:function(newslist){
-      console.log(newslist);
-      $.each(newslist.docs,function(i,user)
-      {
-      $newslist.append('<li><h3>'+user.title+'</h3><img src="'+user.path+'"/><p>'+user.description+'</p> <button name="status" id="click" onclick="onAccept(\''+user._id+'\')"  >Approve</button> &nbsp;&nbsp;&nbsp; <button>Reject</button></li>') 
+$(function() {
+    var $newslist = $('#newslist');
+    $.ajax({
+        type: 'GET',
+        url: '/news/all',
+        success: function(newslist) {
+            console.log(newslist);
+            $.each(newslist.docs, function(i, user) {
+                $newslist.append('<li><h3>' + user.title + '</h3><img src="' + user.path + '"><p>' + user.description + '</p> <button name="status" id="click" onclick="onAccept(\'' + user._id + '\')"  >Approve</button> &nbsp;&nbsp;&nbsp; <button>Reject</button></li>')
+            });
+        }
     });
-  }
-  });
 
 });
