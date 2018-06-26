fda0dbf369b97a3163c4758fd09d1afd6e30bf95
sumathi <vijaya.sumathi@spotcues.com>
2018-06-26 13:05:37 +0530

    Admin login

--- scripts/login.js
+++ scripts/login.js
@@ -1,12 +1,2 @@
-var User = require('../models/adminlogin');
-const adminlogin = new User();
-    adminlogin.username='admin',
-    adminlogin.password= '$2y$12$7PbB.VP79nxNrYkU.9.tfe9MU486md5tiBY1WuZNlR4sjfBGTIbsG',               
-  
 
-adminlogin.save(function (err) {
-    if (err) {
-        console.log(err);
-    }
-    // console.log('LOGIN !' )
-})
+db.adminlogin.insert({username:'admin',password :'$2y$12$7PbB.VP79nxNrYkU.9.tfe9MU486md5tiBY1WuZNlR4sjfBGTIbsG'})
