d59ed0adf2e2ae4ba114269331a1b4fe369c0350
Umesh <umeshrastogi007@gmail.com>
2018-07-12 14:22:49 +0530

    folder deleted

--- umesh/userManager.js
+++
@@ -1,5 +0,0 @@
-UserManager = function(app) {
-    var UserProvider = require('./userProvider-mongodb').UserProvider;
-    var userProvider = new UserProvider(mongoServer, mongoPort);
-    app.set('userProvider', userProvider);
-}
