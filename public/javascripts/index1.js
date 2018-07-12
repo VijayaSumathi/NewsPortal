c048d320646fd36b53d92f6804c276584407a37a
Umesh <umeshrastogi007@gmail.com>
2018-07-12 13:07:39 +0530

    final

--- public/javascripts/index1.js
+++ public/javascripts/index1.js
@@ -1,3 +1,9 @@
+function dtfnct(date) {
+    var d = new Date(date)
+    var weekday = new Array("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday")
+    var monthname = new Array("Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec")
+    return weekday[d.getDay()] + ", " + (d.getDate() + " ") + monthname[d.getMonth()] + " " + (d.getFullYear())
+}
 $(function() {
     var $newslist = $('#approved');
     $.ajax({
@@ -5,10 +11,8 @@ $(function() {
         url: '/news/approve',
         success: function(newslist) {
             console.log(newslist);
-
-
             $.each(newslist.docs, function(i, user) {
-                $newslist.prepend('<li><div class="last"><img  src="' + user.path + '"><p style="font-size:17px; margin-bottom:5px; font-family:Open Sans"><b>' + user.title + '</b></p><p2 style="font-size:16px;font-family:Open Sans">' + user.description + '</p2></div></li>')
+                $newslist.prepend('<li><div class="last"><img  src="' + user.path + '"><p style="font-size:17px; margin-bottom:5px; font-family:Open Sans"><b>' + user.title + '</b></p><p2 style="font-size:16px;font-family:Open Sans">' + user.description + '</p2><div><b>' + dtfnct(user.updated_at) + '</b></div></div></li>')
 
 
             });
